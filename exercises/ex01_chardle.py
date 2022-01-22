1cd5edca2608b7978c24823be97cdfd297512e75
Nicole Clegg <nmclegg@ad.unc.edu>
2022-01-22 15:25:24 -0500

    Progress on Exercies 1

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -3,12 +3,12 @@
 __author__ = "730388398"
 
 
-word: str = input("Enter a 5-character word: ")
-if int(len(word)) != 5:
+user_word: str = input("Enter a 5-character word: ")
+if int(len(user_word)) != 5:
     print("Error: Word must contain 5 characters ")
     exit()
 
-int(len(word)) == 5
+int(len(user_word)) == 5
 character: str = input("Enter a single character: ")
 counter: int = 0
 
@@ -17,28 +17,28 @@ if int(len(character)) != 1:
     exit()
 
 if int(len(character)) == 1:
-    print("Searching for " + character + " in " + word)
+    print("Searching for " + character + " in " + user_word)
     
-if str(word)[0] == character:
+if str(user_word)[0] == character:
     print(character + " found at index 0")
     counter = counter + 1
-if str(word)[1] == character:
+if str(user_word)[1] == character:
     print(character + " found at index 1") 
     counter = counter + 1
-if str(word)[2] == character:
+if str(user_word)[2] == character:
     print(character + " found at index 2")
     counter = counter + 1
-if str(word)[3] == character: 
+if str(user_word)[3] == character: 
     print(character + " found at index 3")
     counter = counter + 1
-if str(word)[4] == character:
+if str(user_word)[4] == character:
     print(character + " found at index 4")
     counter = counter + 1
     
 if counter >= 1: 
     if counter == 1: 
-        print("1 instance of " + character + " found in " + word)
+        print("1 instance of " + character + " found in " + user_word)
     else: 
-        print(str(counter) + " instances of " + character + " found in " + word) 
+        print(str(counter) + " instances of " + character + " found in " + user_word) 
 else: 
-    print("No instances of " + character + " found in " + word)  
+    print("No instances of " + character + " found in " + user_word)  
