6c6e019782dff91fecb31fa77f0aea9768c4486e
Nicole Clegg <nmclegg@ad.unc.edu>
2022-02-23 20:28:46 -0500

    progress on 5

--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,7 +1,7 @@
 """Test Utils Functions.""" 
 
 
-from exercises.ex05.utils import only_evens 
+from exercises.ex05.utils import only_evens, sub, concat
 
 
 def test_only_evens_edge() -> None:
@@ -10,10 +10,30 @@ def test_only_evens_edge() -> None:
 
 
 def test_only_evens_use() -> None: 
-    numbers: list[int] = [0, 1, 2, 3]
+    numbers: list[int] = [-2, -1, 0, 1, 2, 3, 4, 5, 6]
     assert only_evens(numbers)
 
 
 def test_only_evens_use2() -> None:
-    numbers: list[int] = [4, 9, 2, 12]
-    assert only_evens(numbers) 
+    numbers: list[int] = [-4, -9, 2, -12]
+    assert only_evens(numbers) == [] 
+
+
+def test_sub_edge() -> None:
+    sub_list: list[int] = []
+    first: int = 1
+    last: int = 3
+    assert sub(sub_list, first, last) == [10, 20, 30]
+
+
+def test_sub_use() -> None:
+    sub_list: list[int] = [-2, -1, 0, 2, 3, 4, 5, 6]
+    first: int = 1
+    last: int = 7
+    assert sub(sub_list, first, last)
+
+
+def test_concat() -> None: 
+    evens_list: list[int] = [-2, -1, 0, 1, 2, 3, 4, 5, 6]
+    subs_list: list[int] = [-2, -1, 0, 2, 3, 4, 5, 6]
+    assert concat(evens_list, subs_list)
