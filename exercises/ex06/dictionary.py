bb04b600cbc366629399a5937ca77918d8beeacf
Nicole Clegg <nmclegg@ad.unc.edu>
2022-03-02 21:06:00 -0500

    progress of ex06

--- exercises/ex06/dictionary.py
+++ exercises/ex06/dictionary.py
@@ -8,9 +8,9 @@ def invert(given_invert: dict[str, str]) -> dict[str, str]:
     """Given the input of keys and their values, the output needs to swap them."""
     input_swap: dict[str, str] = {}
     for key in given_invert: 
-        new_key = given_invert[key]
+        new_key = given_invert[key]  # this takes the keys from the given_invert dict and assigns them to the new dict. I did this instead of creating a sepreate list that I would then have to append keys to.
         if new_key in input_swap:
-            raise KeyError("error")
+            raise KeyError("Error: Repeated Key.")
         else:
             input_swap[given_invert[key]] = key
     return input_swap
@@ -19,15 +19,15 @@ def invert(given_invert: dict[str, str]) -> dict[str, str]:
 def favorite_color(colors: dict[str, str]) -> str: 
     """Input the users' names and their favorite colors to see the most common favorite color."""
     if colors == dict():
-        return 'Your dictionary is empty!'
+        return 'Dictionary is empty.'  # If given dict of colors is empty, an error message that notifies the user is returned.
     result: dict[str, int] = {}
     counter: int = 0
-    counter_color: str = ""
+    counter_color: str = ""  # this empty str will return the most common color from the given dict.
     for key in colors:
         if colors[key] in result:
-            result[colors[key]] += 1
+            result[colors[key]] += 1  # This if statement will count the reoccuring values of colors from the the dict.
         else:
-            result[colors[key]] = 1
+            result[colors[key]] = 1  # one count of color values from dict. 
     for key in result:
         if result[key] > counter:
             counter_color = key
--- exercises/ex06/dictionary_test.py
+++ exercises/ex06/dictionary_test.py
@@ -12,31 +12,31 @@ import pytest
 def test_invert() -> None:
     """Test a dictionary."""
     given_invert: dict[str, str] = {'a': 'z', 'b': 'y', 'c': 'x'}
-    assert(invert.given_invert)
+    assert(given_invert)
 
 
 def test_invert_use() -> None:
     """Test a dictionary."""
     given_invert: dict[str, str] = {'george': 'gogy', 'louis': 'lou', 'harold': 'harry'}
-    assert(invert.given_invert)
+    assert invert(given_invert)
 
 
 def test_favorite_colors() -> None:
     """Test."""
-    colors: dict = {"george": "blue", "louis": "blue", "alex": "red", "karl": "purple", "clay": "green", "nick": "red", "nicole": "blue"}
-    assert(favorite_color.colors)
+    colors: dict[str, str] = {"george": "blue", "louis": "blue", "alex": "red", "karl": "purple", "clay": "green", "nick": "red", "nicole": "blue"}
+    assert favorite_color(colors)
 
 
 def test_favorite_colors_use() -> None:
     """Test."""
     colors: dict = {"emma": "green", "nicole": "green", "sarah": "pink", "george": "blue"}
-    assert(favorite_color.colors)
+    assert favorite_color(colors)
 
 
 def test_favorite_colors_tie() -> None:
     """Test colors for a tie."""
-    colors: dict = {"emma": "green", "nicole": "green", "sarah": "pink", "george": "blue", "clay": "blue"}
-    assert(favorite_color.colors)
+    colors: dict[str, str] = {"emma": "green", "nicole": "green", "sarah": "pink", "george": "blue", "clay": "blue"}
+    assert favorite_color(colors)
 
 
 with pytest.raises(KeyError):
@@ -46,17 +46,17 @@ with pytest.raises(KeyError):
 
 def test_counter() -> None:
     """Test for list."""
-    values: list = ["berries", "berries", "veggies", "protein", "water", "berries", "water"]
-    assert(count.values)
+    values: list[str] = ["berries", "berries", "veggies", "protein", "water", "berries", "water"]
+    assert count(values)
 
 
 def test_counter_small() -> None:
     """Test for small values."""
-    values: list = ["a", "b", "a", "c", "d"]
-    assert(count.values)
+    values: list[str] = ["a", "b", "a", "c", "d"]
+    assert count(values)
 
 
 def test_counter_large() -> None:
     """Test for large values."""
-    values: list = ["z", "a", "y", "b", "x", "c", "q", "z", "z", "z", "z", "z", "a", "q", "q", "x"]
-    assert(count.values)
+    values: list[str] = ["z", "a", "y", "b", "x", "c", "q", "z", "z", "z", "z", "z", "a", "q", "q", "x"]
+    assert count(values)
