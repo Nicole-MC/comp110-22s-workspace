c39974e94b96d8ca60a52c6da4df7f41ec954cc0
Nicole Clegg <nmclegg@ad.unc.edu>
2022-03-02 19:40:03 -0500

    progress on 6

--- exercises/ex06/dictionary.py
+++ exercises/ex06/dictionary.py
@@ -6,34 +6,41 @@ __author__ = "730388398"
 
 def invert(given_invert: dict[str, str]) -> dict[str, str]: 
     """Given the input of keys and their values, the output needs to swap them."""
-    input_swap = {}
-    for key, value in given_invert: 
-        input_swap.values = key
-        input_swap.keys = value
+    input_swap: dict[str, str] = {}
+    for key in given_invert: 
+        new_key = given_invert[key]
+        if new_key in input_swap:
+            raise KeyError("error")
+        else:
+            input_swap[given_invert[key]] = key
     return input_swap
 
 
 def favorite_color(colors: dict[str, str]) -> str: 
     """Input the users' names and their favorite colors to see the most common favorite color."""
-    color_counter_dict = {}
-    for key, value in colors.items():
-        if key not in color_counter_dict:
-            color_counter_dict[value] = 0
+    if colors == dict():
+        return 'Your dictionary is empty!'
+    result: dict[str, int] = {}
+    counter: int = 0
+    counter_color: str = ""
+    for key in colors:
+        if colors[key] in result:
+            result[colors[key]] += 1
         else:
-            color_counter_dict[value] += 1
-    return max(color_counter_dict)
+            result[colors[key]] = 1
+    for key in result:
+        if result[key] > counter:
+            counter_color = key
+            counter = result[key]
+    return counter_color
 
 
 def count(values: list[str]) -> dict[str, int]: 
     """Input a list of words to see how many times each one is inputed."""
-    counter_dict = {}
-    i = 0
-    for i in range(len(values)):
-        counter_dict[i] = values[i]
-        i += 1
-    for key, value in counter_dict.items():
-        if key not in counter_dict:
-            counter_dict[value] = 0
-        else: 
+    counter_dict: dict[str, int] = dict()
+    for value in values:
+        if value not in counter_dict:
+            counter_dict[value] = 1
+        else:
             counter_dict[value] += 1
     return counter_dict
--- exercises/ex06/dictionary_test.py
+++ exercises/ex06/dictionary_test.py
@@ -12,31 +12,31 @@ import pytest
 def test_invert() -> None:
     """Test a dictionary."""
     given_invert: dict[str, str] = {'a': 'z', 'b': 'y', 'c': 'x'}
-    assert(given_invert)
+    assert(invert.given_invert)
 
 
 def test_invert_use() -> None:
     """Test a dictionary."""
     given_invert: dict[str, str] = {'george': 'gogy', 'louis': 'lou', 'harold': 'harry'}
-    assert(given_invert)
+    assert(invert.given_invert)
 
 
 def test_favorite_colors() -> None:
     """Test."""
     colors: dict = {"george": "blue", "louis": "blue", "alex": "red", "karl": "purple", "clay": "green", "nick": "red", "nicole": "blue"}
-    assert(colors)
+    assert(favorite_color.colors)
 
 
 def test_favorite_colors_use() -> None:
     """Test."""
     colors: dict = {"emma": "green", "nicole": "green", "sarah": "pink", "george": "blue"}
-    assert(colors)
+    assert(favorite_color.colors)
 
 
 def test_favorite_colors_tie() -> None:
     """Test colors for a tie."""
     colors: dict = {"emma": "green", "nicole": "green", "sarah": "pink", "george": "blue", "clay": "blue"}
-    assert(colors)
+    assert(favorite_color.colors)
 
 
 with pytest.raises(KeyError):
@@ -47,15 +47,16 @@ with pytest.raises(KeyError):
 def test_counter() -> None:
     """Test for list."""
     values: list = ["berries", "berries", "veggies", "protein", "water", "berries", "water"]
-    assert(values)
+    assert(count.values)
 
 
 def test_counter_small() -> None:
     """Test for small values."""
     values: list = ["a", "b", "a", "c", "d"]
-    assert(values)
+    assert(count.values)
+
 
 def test_counter_large() -> None:
     """Test for large values."""
     values: list = ["z", "a", "y", "b", "x", "c", "q", "z", "z", "z", "z", "z", "a", "q", "q", "x"]
-    assert(values)
+    assert(count.values)
